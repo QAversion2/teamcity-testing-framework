a6852fbbf2c2568da63f940be6a3924cfe29a7af
Sergey Akimov <sergey.akimov@selecty.ru>
2024-10-15 18:37:14 +0300

    Добавил всё по уроку "Hегативный тест по созданию билд конфигурации". Копировал почти всё из userCreatesBuildTypeTest и добавил аннотацию Parameterizable в Parameterizable

--- src/main/java/com/example/teamcity/api/models/BuildType.java
+++ src/main/java/com/example/teamcity/api/models/BuildType.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.api.models;
 
+import com.example.teamcity.api.annotations.Parameterizable;
 import com.example.teamcity.api.annotations.Random;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -12,9 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-
 public class BuildType extends BaseModel {
     @Random
+    @Parameterizable
     private String id;
     @Random
     private String name;
