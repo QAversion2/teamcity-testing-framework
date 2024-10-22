d7bc8f89259fd4abefd8682c02c558ae5d32ed28
Sergey Akimov <sergey.akimov@selecty.ru>
2024-10-22 19:18:15 +0300

    Добавил материал из UI модуля. "Создание страницы проекта". + неявное ожидание в классе CreateProjectPage в методе setupProject. Иначе тест падает

---
+++ src/test/java/com/example/teamcity/ui/admin/CreateProjectPage.java
@@ -0,0 +1,34 @@
+package com.example.teamcity.ui.admin;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.example.teamcity.ui.pages.ProjectsPage;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
+public class CreateProjectPage extends CreateBasePage {
+    private static final String PROJECT_SHOW_MODE = "createProjectMenu";
+
+    private SelenideElement projectNameInput = $("#projectName");
+
+    public static CreateProjectPage open(String projectId) {
+        return Selenide.open(CREATE_URL.formatted(projectId, PROJECT_SHOW_MODE), CreateProjectPage.class);
+    }
+
+    public CreateProjectPage createForm(String url) {
+        baseCreateForm(url);
+        return this;
+    }
+
+    public ProjectsPage setupProject(String projectName, String buildTypeName) {
+        projectNameInput.shouldBe(Condition.visible, Duration.ofSeconds(15));
+        projectNameInput.val(projectName);
+        buildTypeNameInput.val(buildTypeName);
+        submitButton.click();
+        return page(ProjectsPage.class);
+    }
+}
